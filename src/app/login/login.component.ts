7a8563fcdcb00a6b02ea347557a32c2db76bb672
Jyothika Sankar <134379479+1Jy-svg@users.noreply.github.com>
2024-03-31 23:45:48 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgClass } from '@angular/common';
@@ -10,71 +10,44 @@ import { NgClass } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  isSignDivVisiable: boolean  = true;
-  signUpObj: SignUpModel  = new SignUpModel();
-  loginObj: LoginModel  = new LoginModel();
+export class LoginComponent implements OnInit {
+  signupUsers : any[]=[];
+  signupObj:any={
+    username:'',
+    email:'',
+    password:''
+  };
+  loginObj:any={
+    email:'',
+    password:''
+  };
 
-  constructor(private router: Router){}
-
-
-  onRegister() {
-    debugger;
-    const localUser = localStorage.getItem('angular17users');
-    if(localUser != null) {
-      const users =  JSON.parse(localUser);
-      users.push(this.signUpObj);
-      localStorage.setItem('angular17users', JSON.stringify(users))
-    } else {
-      const users = [];
-      users.push(this.signUpObj);
-      localStorage.setItem('angular17users', JSON.stringify(users))
-    }
-    
-  }
-
-  onLogin() {
-    debugger;
-    const localUsers =  localStorage.getItem('angular17users');
-    if(localUsers != null) {
-      const users =  JSON.parse(localUsers);
-
-      const isUserPresent =  users.find( (user:SignUpModel)=> user.email == this.loginObj.email && user.password == this.loginObj.password);
-      if(isUserPresent != undefined) {
-        localStorage.setItem('loggedUser', JSON.stringify(isUserPresent));
-        this.router.navigateByUrl('/dashboard');
-      } else {
-        alert("No User Found")
-      }
+  constructor(){}
+  ngOnInit(): void {
+    const localData=localStorage.getItem('signUpUsers');
+    if(localData!= null){
+      this.signupUsers=JSON.parse(localData);
     }
   }
-
-}
-
-export class SignUpModel  {
-  name: string;
-  email: string;
-  password: string;
-  phone: string;
-  address: string;
-
-  constructor() {
-    this.email = "";
-    this.name = "";
-    this.phone="";
-    this.address="";
-    this.password= ""
+  onSignup(){
+       this.signupUsers.push(this.signupObj);
+       localStorage.setItem('signUpUsers',JSON.stringify(this.signupUsers));
+       this.signupObj={
+        username:'',
+        email:'',
+        password:''
+      };
   }
-}
-
-export class LoginModel  { 
-  email: string;
-  password: string;
-phone: any;
-address: any;
-
-  constructor() {
-    this.email = ""; 
-    this.password= ""
+  onlogin(){
+      const isUserExist =this.signupUsers.find(m => m.username==this.loginObj.username && 
+        m.password==this.loginObj.password);
+        if(isUserExist != undefined){
+          alert('user Login succesfully');
+        }
+        else{
+          alert('Wrong credentials');
+        }
   }
+
+
 }
